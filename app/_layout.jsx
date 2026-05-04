6a74399febb03b2b11d094fd9da7bc3e4bf8345f
Lara Peddinghausen <larapedd@gmail.com>
2026-05-03 23:01:58 -0300

    feat(navigation): adiciona drawer

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,15 +1,68 @@
-import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+import { View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { router } from 'expo-router';
 
 export default function Layout() {
-    return (<Stack>
-        {/* configuração da tela do pomodoro. É opcional. */}
-        <Stack.Screen
-            name="index"
-            options={{ headerShown: false }}
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#021123',
+          },
+          headerTintColor: '#fff',
+          drawerStyle: {
+            backgroundColor: '#021123'
+          },
+          drawerLabelStyle: {
+            color: '#fff',
+          }
+        }}>
+
+        <Drawer.Screen
+          name='index'
+          options={{
+            headerShown: false,
+            drawerItemStyle: { display: 'none' }
+          }}
         />
-        <Stack.Screen
-            name='pomodoro'
-            options={{ headerShown: false }}
+
+        <Drawer.Screen
+          name='add-task/index'
+          options={{
+            drawerItemStyle: { display: 'none' },
+            title: '',
+            headerLeft: () => {
+              return <Ionicons  //já funciona como botão. Não precisa colocar dentro de um pressable
+              name='arrow-back'
+              size={24}
+              color='#fff'
+              style={{marginLeft: 16}}
+              onPress={() => router.navigate('/tasks')}
+              />
+            }
+          }}
         />
-    </Stack>);
+
+        <Drawer.Screen
+          name='pomodoro'
+          options={{
+            drawerLabel: 'Timer',
+            title: ''
+          }}
+        />
+
+        <Drawer.Screen
+          name='tasks/index'
+          options={{
+            drawerLabel: 'Lista de tarefas',
+            title: ''
+          }}
+        />
+
+      </Drawer>
+    </GestureHandlerRootView>
+  );
 }
