ec264411d9bd2373018bf7c3dcbd521477be8dcc
Lara Peddinghausen <larapedd@gmail.com>
2026-05-19 17:04:05 -0300

    feat(context): implementa contexto global de tarefas com persistência local

--- components/context/TaskProvider.jsx
+++ components/context/TaskProvider.jsx
@@ -1,18 +1,52 @@
-import { createContext, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createContext, useEffect, useState } from "react";
 
 
 export const TaskContext = createContext()
 
-export function TasksProvider() {
+const TASKS_STORAGE_KEY = 'fokus-tasks'
+
+export function TasksProvider({ children }) {
 
     const [tasks, setTasks] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const jsonValue = await AsyncStorage.getItem(TASKS_STORAGE_KEY);
+                const loadedData = jsonValue != null ? JSON.parse(jsonValue) : [];
+                setTasks(loadedData)
+                setIsLoaded(true)
+            } catch (e) {
+                // error reading value
+            }
+        };
+        getData()
+    }, [])
+
+    useEffect(() => {
+        const storeData = async (value) => {
+            try {
+                const jsonValue = JSON.stringify(value);
+                await AsyncStorage.setItem(TASKS_STORAGE_KEY, jsonValue);
+            } catch (e) {
+                // saving error
+            }
+        };
+        if (isLoaded) {
+            storeData(tasks)
+        }
+    }, [tasks])
 
     const addTask = (description) => {
+        console.log('Tarefa vai ser adicionada')
         setTasks(oldState => {
             return [
                 ...oldState,
                 {
-                    description: '',
+                    description,
                     id: oldState.length + 1
                 }
             ]
@@ -30,7 +64,7 @@ export function TasksProvider() {
         })
     }
 
-    const deleteTaskCompleted = (id) => {
+    const deleteTask = (id) => {
         setTasks(oldState => {
             return oldState.filter(t => t.id != id)
         })
@@ -41,7 +75,7 @@ export function TasksProvider() {
             tasks,
             addTask,
             toggleTaskCompleted,
-            deleteTaskCompleted
+            deleteTask
 
         }}>
             {children}
